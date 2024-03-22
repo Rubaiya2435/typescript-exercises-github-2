8d279f2025aa99b9879e0239535c3c4e2f33c11b
Rubaiya2435 <164077424+Rubaiya2435@users.noreply.github.com>
2024-03-22 13:41:51 -0300

    typescript exercise 2

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famousPerson = "Quaid-e-Azam";
+let quote = "I don't believe in taking the right decision, I take a decision and make it right.";
+console.log(`${famousPerson}, once said: "${quote}"`);
---
+++ index.ts
@@ -0,0 +1,3 @@
+let famousPerson: string = "Quaid-e-Azam";
+let quote: string = "I don't believe in taking the right decision, I take a decision and make it right.";
+console.log(`${famousPerson}, once said: "${quote}"`);
